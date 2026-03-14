8e1bac68255359fd3b2e05f7db26ce1215b0fd3a
Altair343 <and97_26@hotmail.com>
2026-03-13 19:23:52 -0600

    [ADD] lab-2

--- src/data/laboratory.ts
+++ src/data/laboratory.ts
@@ -13,6 +13,30 @@ export interface LaboratoryEntry {
 }
 
 export const laboratoryEntries: LaboratoryEntry[] = [
+
+    {
+		id: 'lab-2',
+		title: 'Programación creativa con JavaScript',
+		description:
+			'Proyecto realizado en JavaScript del curso Programación creativa con JavaScript, se trata de piezas visuales generadas con código, utilizando la etiqueta canvas de HTML5, se pueden encontrar diferentes piezas visuales con diferentes técnicas y estilos, el objetivo es experimentar con el código para crear piezas visuales interesantes.',
+		thumbnail:
+			'https://res.cloudinary.com/denebv283c/image/upload/v1773450837/piezas_visuales/468607_hnyktw.png',
+		images: [
+            'https://res.cloudinary.com/denebv283c/image/upload/v1773450837/piezas_visuales/468607_hnyktw.png',
+			'https://res.cloudinary.com/denebv283c/image/upload/v1773450836/piezas_visuales/468601_rgx4ih.png',
+            'https://res.cloudinary.com/denebv283c/image/upload/v1773450836/piezas_visuales/468602_oxfxbt.png',
+            'https://res.cloudinary.com/denebv283c/image/upload/v1773450837/piezas_visuales/468606_d7uqmq.png',
+            'https://res.cloudinary.com/denebv283c/image/upload/v1773450837/piezas_visuales/468603_vynoku.png',
+            'https://res.cloudinary.com/denebv283c/image/upload/v1773450837/piezas_visuales/468604_viaiky.png',
+            'https://res.cloudinary.com/denebv283c/image/upload/v1773450837/piezas_visuales/468605_dx5kxa.png',
+            'https://res.cloudinary.com/denebv283c/image/upload/v1773450837/piezas_visuales/468608_xfjxb9.png',
+            'https://res.cloudinary.com/denebv283c/image/upload/v1773450838/piezas_visuales/468609_lcw2v5.png',
+		],
+		technologies: ['JavaScript', 'Canvas', 'HTML5','Nodejs'],
+		githubUrl: 'https://github.com/Altair343/piezas_visuales',
+		year: '2026',
+	},
+
 	{
 		id: 'lab-1',
 		title: 'World-Jump',
